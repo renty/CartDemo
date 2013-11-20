77cef4ae32bf67ab85f52bb7503b543915e53cc6
SUNMAC <sunmac@SUNMACtekiMac.local>
2013-11-19 21:30:08 -0800

    Initial Commit

---
+++ CartDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CartDemo
+//
+//  Created by SUNMAC on 13-11-19.
+//  Copyright (c) 2013年 lvpw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
